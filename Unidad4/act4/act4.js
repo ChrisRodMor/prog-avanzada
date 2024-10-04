c8b860a5c1ed04e70281df77c9959157f40bf486
Christian Rodriguez <christian2_21@alu.uabcs.mx>
2024-10-04 13:29:14 -0700

    act4CONLESS

--- Unidad4/act4/act4.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const colors = ['#0275D8', '#5DB85B', '#5CC0DE', '#F0AD4E', '#D6524E'];
-    const listItems = document.querySelectorAll('#roleList li');
-
-    listItems.forEach((item, index) => {
-        item.style.backgroundColor = colors[index];
-    });
-});
